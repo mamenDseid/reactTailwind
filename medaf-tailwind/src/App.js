861b6dfc0bd383c31d41006c9f0b1982a661d279
mamenDseid <mamelala@gmail.com>
2022-10-04 16:56:45 +0000

    cv

--- medaf-tailwind/src/App.js
+++ medaf-tailwind/src/App.js
@@ -3,7 +3,7 @@ import HomeScreen from "./screens/HomeScreen";
 export default function App() {
   return (
     <div className=" flex bg-gray-200 mx-auto min-h-screen max-w-2xl justify-center">
-  <HomeScreen/>
+      <HomeScreen />
     </div>
   )
 }
--- medaf-tailwind/src/screens/HomeScreen.js
+++ medaf-tailwind/src/screens/HomeScreen.js
@@ -3,11 +3,11 @@ import { brandData } from '../data/brandData'
 
 export default function HomeScreen() {
     return (
-        <div>
+        <div className='justify-center'>
             <h1 className="text-4xl text-center font-bold ">
                 All Phone Brands
             </h1>
-            <div className='grid grid-cols-4 gap-4 mx-2'>
+            <div className='grid grid-cols-4 gap-4 mx-2 my-auto'>
                 {brandData.map((brand) => (
                     <div className='shadow-md'>
                         <img className=' object-fill' src={brand.bImg} />
