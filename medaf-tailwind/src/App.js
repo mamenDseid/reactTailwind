9121e8fa3e3978f8897f2869bb4052813564025f
mamenDseid <mamelala@gmail.com>
2022-10-04 16:36:17 +0000

    cx

--- medaf-tailwind/src/App.js
+++ medaf-tailwind/src/App.js
@@ -1,7 +1,7 @@
 export default function App() {
   return (
     <div className=" flex bg-gray-200 mx-auto min-h-screen max-w-2xl justify-center">
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-4xl font-bold ">
       Hello world!
     </h1>
     </div>
